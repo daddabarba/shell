499c63f156b28bb371c78e3238b6e1e419d6b315
daddabarba <davidebarbieri97@gmail.com>
2019-03-06 00:17:01 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/main.c.o"
+  "shell.pdb"
+  "shell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+c#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
